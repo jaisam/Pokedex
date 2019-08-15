24753a74e72cf32506d1db1c5a5cad488a4a8976
Jai Samtani <jaisamtani123@gmail.com>
2019-08-15 17:55:17 +0530

    Declared autoCOmpleteArray outside of function

--- src/app/name/name.component.ts
+++ src/app/name/name.component.ts
@@ -7,9 +7,11 @@ import * as localPokemonData from '../pokemonData.json';
   styleUrls: ['./name.component.css']
 })
 export class NameComponent implements OnInit {
-
+ 
   localPokemonDataTs;
   textInput : string;
+   autoCompleteArray  = [];
+  
   /* why this does not work? Code Piece-2
   constructor() { 
     this.localPokemonDataTs = localPokemonData;
@@ -22,6 +24,7 @@ export class NameComponent implements OnInit {
 
   constructor() {
     this.localPokemonDataTs = localPokemonData.data;
+
   }
 
   ngOnInit() {
@@ -34,16 +37,17 @@ export class NameComponent implements OnInit {
 
   autoCompletion(event : any) {
     
-    //console.log(event.target.value);
+    console.log(event.target.value);
     this.textInput = event.target.value ;
-    let autoCompleteArray = [];
+    this.autoCompleteArray.splice(0,this.autoCompleteArray.length);
+
 
     for(let i= 0 ; i < localPokemonData.data.length ; i++){
       
-      if ( localPokemonData.data[i].name.includes(this.textInput,0)){
-      autoCompleteArray.push(localPokemonData.data[i].name);
-        console.log(autoCompleteArray);
-        console.log(typeof(autoCompleteArray));
+      if ( localPokemonData.data[i].name.toLowerCase().includes(this.textInput.toLowerCase(),0)){
+      this.autoCompleteArray.push(localPokemonData.data[i].name);
+        console.log(this.autoCompleteArray);
+        //console.log(typeof(this.autoCompleteArray));
       }
 
     }    
