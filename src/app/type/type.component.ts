c67a1c1377abd46573a1283f020de894a3183c87
jaisam <jaisamtani123@gmail.com>
2019-09-03 23:51:25 +0530

    Completed autoCompleteArray of Type component. Displaying pokemons based on TYPE inputed by User

--- src/app/type/type.component.ts
+++ src/app/type/type.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as localPokemonData from '../pokemonData.json';
 
 @Component({
   selector: 'app-type',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypeComponent implements OnInit {
 
-  constructor() { }
+  localPokemonDataTS;
+  inputValue: String;
+  autoCompleteArray = [];
+
+  constructor() {
+    this.localPokemonDataTS = localPokemonData.data;
+  }
 
   ngOnInit() {
   }
 
+  autoComplete(event: Event) {
+    
+    this.inputValue = (<HTMLInputElement>event.target).value; // Why typecasting is required?
+    this.autoCompleteArray.splice(0, this.autoCompleteArray.length);
+
+    if (this.inputValue !== "") {
+      //console.log((this.inputValue).toUpperCase());
+      //console.log(this.inputValue.length);
+
+      for (let i = 0; i < this.localPokemonDataTS.length; i++) {
+        //console.log((this.localPokemonDataTS[i].name.substring(0,this.inputValue.length)).toUpperCase());
+
+        for (let j = 0; j < this.localPokemonDataTS[i].type.length; j++) {
+          //console.log(this.localPokemonDataTS[i].type[j]);
+
+          if ((this.inputValue).toUpperCase() === (this.localPokemonDataTS[i].type[j].substring(0, this.inputValue.length)).toUpperCase()) {
+            
+            var obj = {
+              id: "",
+              name: ""
+            }
+        
+            obj.id = this.localPokemonDataTS[i].id;
+            obj.name = this.localPokemonDataTS[i].name;
+            console.log(obj.name);
+            this.autoCompleteArray.push(obj);
+            break;
+          }
+        }
+
+      }
+    }
+
+    //   console.log(this.autoCompleteArray.length);
+    //   for(let i=0 ; i< this.autoCompleteArray.length ; i++){
+    //     console.log(this.autoCompleteArray[i].name);
+    // }
+
+  }
 }
